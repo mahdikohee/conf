3b09985790ea3952d56520541f01659f6c732ca4
mahdikohee <mahdikohee@gmail.com>
2025-05-21 12:30:47 +0600

    Update rust_doc.rs

--- rust_doc.rs
+++ rust_doc.rs
@@ -1,1 +1,4 @@
-https://doc.rust-lang.org/stable/
+all rust doc ====> https://doc.rust-lang.org/stable/
+rust std ====> https://doc.rust-lang.org/std
+source => https://github.com/rust-lang/rust
+compiler dev ====> https://rustc-dev-guide.rust-lang.org/
